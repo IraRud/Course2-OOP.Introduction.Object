0d17628c8973e2166d1d586b41c0ce9a415cf15b
rudskaya irina <irarudskaya5@gmail.com>
2022-11-25 14:40:22 +0300

    Homework 3 - task 2

--- src/skypro/java/course2/Car.java
+++ src/skypro/java/course2/Car.java
@@ -8,13 +8,29 @@ public class Car {
     int year;   // год производства
     String country; // страна сборки
 
-    public Car(String brand, String model, int year, String country, String color, double engineVolume) {   // задание 2 из ДЗ-2 было выпонено в ДЗ-1, конструктор создан
-        this.brand = brand;
-        this.model = model;
-        this.year = year;
-        this.country = country;
-        this.color = color;
-        this.engineVolume = engineVolume;
+    public Car(String brand, String model, int year, String country, String color, double engineVolume) {   // добавлена проверка в конструкторе
+        this.brand = checkBrandModelCountry(brand);
+        this.model = checkBrandModelCountry(model);
+
+        if (year <= 0) {    // проверка года производства
+            this.year = 2000;
+        } else {
+            this.year = year;
+        }
+
+        this.country = checkBrandModelCountry(country);
+
+        if (color == null || color.isBlank()) {    // проверка цвета кузова, если null и пустая строка
+            this.color = "белый";
+        } else {
+            this.color = color;
+        }
+
+        if (engineVolume <= 0) {    // проверка объема двигателя
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
     }
 
     @Override
@@ -22,4 +38,12 @@ public class Car {
         return this.brand + " " + this.model + ", " + this.year + " год выпуска, страна сборки — " + this.country + ", цвет кузова — " +
                 this.color + ", объем двигателя — " + this.engineVolume + " л.";
     }
+
+    String checkBrandModelCountry (String str) {    // проверка для марки, модели, страны сборки
+        if (str == null || str.isBlank()) {    // если null и пустые строки
+            str = "default";
+        }
+        return str;
+    }
+
 }
--- src/skypro/java/course2/Main.java
+++ src/skypro/java/course2/Main.java
@@ -32,7 +32,7 @@ public class Main {
         BMW Z8, сборка в Германии в 2021 году, черный цвет кузова, объем — 3,0 л.
         Kia Sportage 4-го поколения, 2018 год выпуска, сборка в Южной Корее, цвет кузова — красный, объем двигателя — 2,4 л.
         Hyundai Avante, сборка в Южной Корее, цвет кузова — оранжевый, объем двигателя — 1,6 л, год выпуска — 2016 год.*/
-        Car firstCar = new Car("Lada", "Granta", 2015,"Россия", "желтый", 1.7);
+        Car firstCar = new Car("  ", "", -2015,null, "", -1.7); // пример для некорректной работы
         System.out.println(firstCar.toString());
         Car secondCar = new Car("Audi", "A8 50 L TDI quattro", 2020,"Германия", "черный", 3.0);
         System.out.println(secondCar.toString());
