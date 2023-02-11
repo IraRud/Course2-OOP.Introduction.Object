68978b3ad73bdf971652584d35d367d18b9ba342
rudskaya irina <irarudskaya5@gmail.com>
2023-02-11 18:08:37 +0300

    Creating new interface

---
+++ src/skypro/java/course2/transport/Competing.java
@@ -0,0 +1,10 @@
+package skypro.java.course2.transport;
+
+public interface Competing {    //"Соревнующийся"
+
+    void pitStop(); // пит-стоп
+
+    void bestLapTime(); // лучшее время прохождения круга
+
+    double maxSpeed();  // максимальная скорость
+}
