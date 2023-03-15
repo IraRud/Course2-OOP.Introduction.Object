83892733fc7e7e658dae3081401dd8677a5975b1
rudskaya irina <irarudskaya5@gmail.com>
2023-03-16 00:49:58 +0300

    Task 3

--- src/skypro/java/course2/functional/Main.java
+++ src/skypro/java/course2/functional/Main.java
@@ -1,12 +1,14 @@
 package skypro.java.course2.functional;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
 //        task1();
-        task2();
+//        task2();
+        task3();
     }
 
     private static void task1() {
@@ -55,6 +57,29 @@ public class Main {
         // проверка
         stringConsumerLE.accept(name1);
         stringConsumerLE.accept(name2);
+    }
+
+    private static void task3() {
+        // переменные для задания 3
+        Double number1 = 2.1;
+        Double number2 = 2.9;
+
+        // округление и приведение к типу Long через анонимный класс
+        Function<Double, Long> doubleLongFunctionAC = new Function<Double, Long>() {
+            @Override
+            public Long apply(Double aDouble) {
+                return Math.round(aDouble);
+            }
+        };
+        // проверка
+        System.out.println(doubleLongFunctionAC.apply(number1));
+        System.out.println(doubleLongFunctionAC.apply(number2));
+
+        // округление и приведение к типу Long через лямбда-выражение
+        Function<Double, Long> doubleLongFunctionLE = Math::round;
+        // проверка
+        System.out.println(doubleLongFunctionLE.apply(number1));
+        System.out.println(doubleLongFunctionLE.apply(number2));
     }
 
 }
